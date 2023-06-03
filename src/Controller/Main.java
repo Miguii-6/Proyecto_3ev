0afb3adcb1c9303a86db792edc23da8235c1bcd3
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-06-03 19:43:15 +0200

    Commit cos paquetes para organizar o futuro do proyecto

---
+++ src/Controller/Main.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import View.Inicio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Inicio in=new Inicio();
+       in.setVisible(true);
+
+       //Primer commit de la rama Miguel
+    }
+    
+}
