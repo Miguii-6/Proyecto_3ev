01e56bfbd82a047a48895dcef038fb80372bc828
Miguel Mariño Martínez <114078229+Miguii-6@users.noreply.github.com>
2023-06-03 20:12:32 +0200

    Commit coas clases para a dificultade

---
+++ src/Model/CDificil.java
@@ -0,0 +1,31 @@
+
+package Model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+public class CDificil {
+    public void preguntasMedias(){
+        
+        try {
+            //para volver conectar a base de datos
+            ResultSet rs=null;   
+            //para a consulta de despois
+            PreparedStatement ps= null;
+            
+            Model.CConexion objetoConexion = new Model.CConexion();
+            
+            //creamos a consulta
+            String consulta="select * from preguntasdificiles";
+            //preparamos a consulta
+            ps=objetoConexion.estableceConexion().prepareStatement(consulta);
+            
+            //que execute a consulta
+            rs = ps.executeQuery();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ERROR: "+e.toString());
+        }
+    }
+}
