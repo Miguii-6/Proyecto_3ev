8c0195f69ed5b7c05d139cceef25814c10d57c65
Samuel Gardón Prieto <114077937+samueldam1@users.noreply.github.com>
2023-06-04 17:38:16 +0200

    Añadidas funcionalidades
    Añadida funcionalidad de seleccionar una pregunta aleatoriamente de una tabla y mostrarla. Añadido selector de dificultad.

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -2,15 +2,13 @@ package Controller;
 
 import View.*;
 
-
 public class Main {
 
     public static void main(String[] args) {
-        Config_BBDD config_BBDD = new Config_BBDD();
         
-        // Hacer visible el JFrame
-        config_BBDD.setVisible(true);
-        
+       Inicio inicio = new Inicio();
+       inicio.setVisible(true);
+       
     }
     
 }
