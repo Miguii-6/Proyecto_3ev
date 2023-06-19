aca25695ba61e397beabafeeda6dfe68e85eaaf8
Miguel Mariño Martínez <114078229+Miguii-6@users.noreply.github.com>
2023-06-19 13:32:23 +0200

    Cambio el observer al controller

---
+++ src/Controller/ObsLogin.java
@@ -0,0 +1,34 @@
+
+
+package Controller;
+
+import View.Login;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JOptionPane;
+
+public class ObsLogin implements Observer {
+    private Login vistaLogin;
+    
+    
+    /**
+     * Metoodo para ver o login
+     * @param vistaLogin 
+     */
+    public ObsLogin(Login vistaLogin) {
+        this.vistaLogin = vistaLogin;
+    }
+
+    /**
+     * oberva cuando se inicia o sesión o usuario
+     * @param o     the observable object.
+     * @param arg   an argument passed to the method.
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+        //Mensaje para si se validou o usuario correctamente
+        JOptionPane.showMessageDialog(null, "Se ha validado el usuario correctamente");
+
+    }
+
+}
